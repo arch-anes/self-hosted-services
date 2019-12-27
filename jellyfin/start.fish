7d54cabc26262a539f2e499450492b44eb647c17
Anes Belfodil <ans.belfodil@gmail.com>
2019-12-26 22:33:15 -0500

    Replace emby with jellyfin

---
+++ jellyfin/start.fish
@@ -0,0 +1,15 @@
+#!/bin/fish
+
+set source_dir (dirname (readlink -m (status --current-filename)))
+
+mkdir -p /storage/jellyfin
+pushd /storage/jellyfin
+mkdir -p media config
+popd
+
+docker volume create -d local-persist -o mountpoint=/storage/jellyfin/config --name=jellyfin_config
+docker volume create -d local-persist -o mountpoint=/storage/jellyfin/media --name=jellyfin_media
+
+pushd $source_dir
+docker-compose up -d
+popd
