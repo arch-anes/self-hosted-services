e4b4b5c0a6ac6ed73f94f2f86faf0f9de9d37274
Anes Belfodil <abelfodil@users.noreply.github.com>
2019-12-29 12:22:37 -0500

    Ansible (#3)
    * Create basic ansible playbooks
    
    * Replace local-persist volumes with simpler volumes
    
    * Add privatebin playbook
    
    * Add remaining playbooks
    
    * Normalize files
    
    * Use rsync for faster transfers
    
    * Rename proxy network to reverse_proxy
    
    * Update readme

--- traefik/start.fish
+++
@@ -1,9 +0,0 @@
-#!/bin/fish
-
-set source_dir (dirname (readlink -m (status --current-filename)))
-
-pushd $source_dir
-touch acme.json && chmod 600 acme.json
-docker network create proxy
-docker-compose up -d
-popd
--- update-all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-for d in $script_dir/*/ ; do
-    pushd "$d"
-    docker-compose pull
-    popd
-done
