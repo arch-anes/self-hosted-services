3292f97a4dd3f7cd71f786bbf72e80cc27b44eda
Anes Belfodil <ans.belfodil@gmail.com>
2021-07-23 23:54:08 +0000

    Add Huginn

---
+++ stacks/huginn/docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+FILE_ENV_VARS="$(env | grep '_FILE=')"
+for env_var in $FILE_ENV_VARS; do
+    var_name="$(echo $env_var | grep -o '.*_FILE=' | sed 's/_FILE=//g')"
+    file_path="$(echo $env_var | grep -o '_FILE=.*' | sed 's/_FILE=//g')"
+    file_content="$(cat $file_path)"
+    [[ ! $? -eq 0 ]] && exit 1 # Exit if last command failed
+    new_var="$(echo $var_name=$file_content)"
+    export $(echo $new_var | xargs)
+done
+
+exec "$@"
